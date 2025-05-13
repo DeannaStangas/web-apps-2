019412c17f3ba79945fe5d7f5ce961ac50c628b9
Deanna Stangas <deanna.stangas@kellogg.northwestern.edu>
2025-05-13 16:00:19 +0000

    show and add company

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
 
   resources "companies"
   # get("/companies", {:controller => "companies", :action => "index"})
+  # get("/companies"/:id", {:controller => "companies", :action => "show"})
+  # get("/companies"/new", {:controller => "companies", :action => "new"})
+  # post ("/companies", {:controller=> "companies", :action => "create"})
+
   resources "contacts"
   # get("/contacts", {:controller => "contacts", :action => "index"})
 
